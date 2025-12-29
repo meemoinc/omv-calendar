a299f64d66765e432a5df9aefd9eb8321965ee36
Code MeemoInc <34594747+codemeemoinc@users.noreply.github.com>
2025-12-29 13:14:12 +0500

    Update next.config.ts
    CSP, XCTO, Permission Policy set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim();
+
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
